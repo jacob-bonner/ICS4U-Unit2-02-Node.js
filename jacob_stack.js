42bb564764f222aeb0e1db31d91e6675e2dbfdb6
Ubuntu <ubuntu@ip-172-31-8-163.ec2.internal>
2020-12-09 15:55:53 +0000

    Initial commit

--- jacob_stack.js
+++ jacob_stack.js
@@ -5,28 +5,28 @@
 class JacobStack {
   // Constructor that defines stack array
   constructor() {
-    this._STACK_AS_ARRAY = [];
+    this.STACK_AS_ARRAY = [];
   }
 
   // This method adds user input to stack
   pushNumber(inputNumber) {
-    this._STACK_AS_ARRAY.push(inputNumber);
+    this.STACK_AS_ARRAY.push(inputNumber);
   }
 
   // This method pops the item on top of the stack and returns it
   popNumber() {
-    let poppedNumber = this._STACK_AS_ARRAY[this._STACK_AS_ARRAY.length - 1];
-    this._STACK_AS_ARRAY.splice(this._STACK_AS_ARRAY[this
-                                ._STACK_AS_ARRAY.length - 1], 1);
+    let poppedNumber = this.STACK_AS_ARRAY[this.STACK_AS_ARRAY.length - 1];
+    this.STACK_AS_ARRAY.splice(this.STACK_AS_ARRAY[this
+                                .STACK_AS_ARRAY.length - 1], 1);
     return poppedNumber;
   }
 
   // This getter tells the user the most recent itteration of the array
   currentStack() {
     let currentValues = "Current ArrayList: ";
-    for (let counter = 0; counter < this._STACK_AS_ARRAY.length - 1;
+    for (let counter = 0; counter < this.STACK_AS_ARRAY.length - 1;
          counter++) {
-      currentValues = currentValues + this._STACK_AS_ARRAY[counter] + ", ";
+      currentValues = currentValues + this.STACK_AS_ARRAY[counter] + ", ";
     }
     return currentValues;
   }
